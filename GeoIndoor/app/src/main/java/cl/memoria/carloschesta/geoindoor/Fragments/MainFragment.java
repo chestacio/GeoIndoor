6404b3fc481ecaf42892f9b6a6c1e10620f39396
Carlos Chesta <karlos.hetfield@gmail.com>
2017-03-18 20:29:10 -0300

    Coord text on top and limit map view bound

--- GeoIndoor/app/src/main/java/cl/memoria/carloschesta/geoindoor/Fragments/MainFragment.java
+++ GeoIndoor/app/src/main/java/cl/memoria/carloschesta/geoindoor/Fragments/MainFragment.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -17,6 +18,7 @@ import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.TileOverlay;
 import com.google.android.gms.maps.model.TileOverlayOptions;
 
@@ -39,13 +41,11 @@ public class MainFragment extends Fragment implements OnMapReadyCallback{
 
     private GoogleMap gMap;
     private MapView mMapView;
-    private TileOverlayOptions opts;
-    private File parkingFile;
-    private MapBoxOfflineTileProvider provider;
-    private TileOverlay overlay;
-    private AssetManager am;
-    private InputStream is;
     private File f;
+    private TextView tvX;
+    private TextView tvY;
+    private final int MAX_ZOOM = 14;
+    private final int MIN_ZOOM = 11;
 
 
     public MainFragment() {
@@ -56,29 +56,15 @@ public class MainFragment extends Fragment implements OnMapReadyCallback{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        /**
-        am = getActivity().getAssets();
-        try {
-            is = am.open("parking.mbtiles");
-        } catch (IOException e) {
-            Log.e("Parking.mbtiles", "Ocurrio un error al leer el archivo");
-            e.printStackTrace();
-        }
-
-        try {
-            file = new File("parking.mbtiles");
-            FileUtils.copyInputStreamToFile(is, file);
-        } catch (IOException e) {
-            Log.e("Convertir archivo", "Ocurrio un error al convertir el archivo");
-            e.printStackTrace();
-        }
-        */
-
+        // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_main, container, false);
+
+        tvX = (TextView) v.findViewById(R.id.tvX);
+        tvY = (TextView) v.findViewById(R.id.tvY);
 
         mMapView = (MapView) v.findViewById(R.id.map);
         mMapView.onCreate(savedInstanceState);
-        mMapView.onResume();
+        mMapView.onResume(); // needed to get the map to display immediately
         mMapView.getMapAsync(this);
 
         try {
@@ -87,7 +73,7 @@ public class MainFragment extends Fragment implements OnMapReadyCallback{
             e.printStackTrace();
         }
 
-
+        // Get a File from Assets reference to the MBTiles file.
         f = new File(getActivity().getCacheDir()+"/parking.mbtiles");
         if (!f.exists()) try {
 
@@ -97,37 +83,53 @@ public class MainFragment extends Fragment implements OnMapReadyCallback{
             is.read(buffer);
             is.close();
 
-
             FileOutputStream fos = new FileOutputStream(f);
             fos.write(buffer);
             fos.close();
         } catch (Exception e) { throw new RuntimeException(e); }
 
-        //map = ((SupportMapFragment) getActivity().getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
-
-        // Inflate the layout for this fragment
         return v;
     }
 
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        // Retrieve GoogleMap instance from MapFragment or elsewhere
         gMap = googleMap;
         setUpMap();
-        opts = new TileOverlayOptions();
-        provider = new MapBoxOfflineTileProvider(f);
+
+        // Create new TileOverlayOptions instance.
+        TileOverlayOptions opts = new TileOverlayOptions();
+
+        // Create an instance of MapBoxOfflineTileProvider.
+        MapBoxOfflineTileProvider provider = new MapBoxOfflineTileProvider(f);
+
+        // Set the tile provider on the TileOverlayOptions.
         opts.tileProvider(provider);
-        overlay = gMap.addTileOverlay(opts);
+
+        // Add the tile overlay to the map.
+        TileOverlay overlay = gMap.addTileOverlay(opts);
     }
 
     private void setUpMap() {
         gMap.setMapType(GoogleMap.MAP_TYPE_NONE);
-        //gMap.setMyLocationEnabled(true);
-        //gMap.setTrafficEnabled(true);
-        //gMap.setIndoorEnabled(true);
-        //gMap.setBuildingsEnabled(true);
         gMap.getUiSettings().setZoomControlsEnabled(true);
-        CameraUpdate upd = CameraUpdateFactory.newLatLngZoom(new LatLng(-0.106087,-53.766060), 13);
+
+        // Initial camera position
+        CameraUpdate upd = CameraUpdateFactory.newLatLngZoom(new LatLng(-0.106087,-53.766060), 12);
         gMap.moveCamera(upd);
+        gMap.setMaxZoomPreference(MAX_ZOOM);
+        gMap.setMinZoomPreference(MIN_ZOOM);
+        LatLngBounds bounds = new LatLngBounds(new LatLng(-0.09, -53.75), new LatLng(-0.02,-53.355));
+        gMap.setLatLngBoundsForCameraTarget(bounds);
+
+        gMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                tvX.setText(String.valueOf(latLng.longitude));
+                tvY.setText(String.valueOf(latLng.latitude));
+            }
+        });
+
     }
 }
